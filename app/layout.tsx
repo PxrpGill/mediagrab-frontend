beb06d81632703a23f053659128090b7f20a610e
evgeniy.gilyazetdinov <jhongil3007@gmail.com>
2025-07-13 00:23:22 +0300

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,38 @@ const RootLayout = ({
 }>) => {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/assets/favicons/apple-touch-icon.png"
+        />
+         <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/favicons/favicon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/favicons/favicon-512x512.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/assets/favicons/site.webmanifest" />
+      </head>
       <body className={MontserratFonts.className}>
         <ThemeProvider>
           <LayoutProvider>
