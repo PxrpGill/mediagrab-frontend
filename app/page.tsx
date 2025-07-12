a549ca976ae978387a15bef2e63d155f28198e62
evgeniy.gilyazetdinov <jhongil3007@gmail.com>
2025-07-13 00:15:35 +0300

    feat: add metadata for main-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,18 @@
+import type { Metadata } from "next";
+
 import { Home } from "@/views/home";
+
+export const metadata: Metadata = {
+  title: "MediaGrab - Главная",
+  description:
+    "Скачивайте видео и музыку из ВКонтакте и YouTube в один клик! Просто вставьте ссылку - получите MP4, MP3 в HD качестве. Без регистрации, без ограничений. Работает на всех устройствах!",
+  keywords: [
+    "скачать видео вк",
+    "скачать музыку youtube",
+    "конвертер youtube в mp3",
+    "загрузчик vk видео",
+  ],
+};
 
 const HomePage = () => {
   return <Home />;
