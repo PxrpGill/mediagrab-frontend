e6c1ef3b23b0eff2294d103527f1c2302c0d5332
evgeniy.gilyazetdinov <jhongil3007@gmail.com>
2025-07-12 17:24:06 +0300

    feat: add loader

---
+++ features/downloader/ui/downloader-panel/index.tsx
@@ -0,0 +1,5 @@
+import css from "./index.module.css";
+
+export const DownloaderPanel = () => {
+  return <section className={css.root}></section>;
+};
--- features/downloader/ui/search-form/index.tsx
+++ features/downloader/ui/search-form/index.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+// import cx from "clsx";
+
 import { SEARCH_FORM_CONSTANTS } from "@/features/downloader/models/search-form.constants";
+// import LoaderIcon from "@/public/assets/icons/loader.svg";
 import SearchIcon from "@/public/assets/icons/search.svg";
 
 import css from "./index.module.css";
@@ -13,6 +16,7 @@ export const SearchForm = () => {
       <input className={css.input} placeholder={placeholder} type="text" />
       <button type="submit" className={css.searchButton}>
         <SearchIcon className={css.searchIcon} />
+        {/* <LoaderIcon className={cx(css.searchIcon, css.loader)} /> */}
       </button>
     </form>
   );
